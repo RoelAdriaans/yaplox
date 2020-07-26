fb90ba3865decdefbdcaf3be6285e0d701b68bcb
Roel Adriaans <roel@road-support.nl>
2020-07-26 04:31:20 +0200

    Better (?) way to store the enums

--- src/yaplox/token_type.py
+++ src/yaplox/token_type.py
@@ -1,61 +1,60 @@
-from enum import Enum
+import enum
 
 
-class TokenType(Enum):
+class TokenType(enum.Enum):
     """
-    Tokens that are used in the Language,
+    Tokens that are used in the Language. The mapping from Enum to character
+    will be done in the scanner.
     """
-    # Using an Enum like this feels horrible. This can go directly to the dailywtf.com
-    # @TODO Figure out a better way to do this.
     #
     # Single - character tokens.
     #
-    LEFT_PAREN = 0
-    RIGHT_PAREN = 1
-    LEFT_BRACE = 2
-    RIGHT_BRACE = 3
-    COMMA = 4
-    DOT = 5
-    MINUS = 6
-    PLUS = 7
-    SEMICOLON = 8
-    SLASH = 9
-    STAR = 10
+    LEFT_PAREN = enum.auto()
+    RIGHT_PAREN = enum.auto()
+    LEFT_BRACE = enum.auto()
+    RIGHT_BRACE = enum.auto()
+    COMMA = enum.auto()
+    DOT = enum.auto()
+    MINUS = enum.auto()
+    PLUS = enum.auto()
+    SEMICOLON = enum.auto()
+    SLASH = enum.auto()
+    STAR = enum.auto()
     #
     # One or two character tokens.
     #
-    BANG = 11
-    BANG_EQUAL = 12
-    EQUAL = 13
-    EQUAL_EQUAL = 14
-    GREATER = 15
-    GREATER_EQUAL = 16
-    LESS = 17
-    LESS_EQUAL = 18
+    BANG = enum.auto()
+    BANG_EQUAL = enum.auto()
+    EQUAL = enum.auto()
+    EQUAL_EQUAL = enum.auto()
+    GREATER = enum.auto()
+    GREATER_EQUAL = enum.auto()
+    LESS = enum.auto()
+    LESS_EQUAL = enum.auto()
     #
     # Literals.
     #
-    IDENTIFIER = 20
-    STRING = 21
-    NUMBER = 22
+    IDENTIFIER = enum.auto()
+    STRING = enum.auto()
+    NUMBER = enum.auto()
     #
     # Keywords.
     #
-    AND = 30
-    CLASS = 31
-    ELSE = 32
-    FALSE = 33
-    FUN = 34
-    FOR = 35
-    IF = 36
-    NIL = 37
-    OR = 38
-    PRINT = 39
-    RETURN = 40
-    SUPER = 41
-    THIS = 42
-    TRUE = 43
-    VAR = 44
-    WHILE = 45
+    AND = enum.auto()
+    CLASS = enum.auto()
+    ELSE = enum.auto()
+    FALSE = enum.auto()
+    FUN = enum.auto()
+    FOR = enum.auto()
+    IF = enum.auto()
+    NIL = enum.auto()
+    OR = enum.auto()
+    PRINT = enum.auto()
+    RETURN = enum.auto()
+    SUPER = enum.auto()
+    THIS = enum.auto()
+    TRUE = enum.auto()
+    VAR = enum.auto()
+    WHILE = enum.auto()
 
-    EOF = 46
+    EOF = enum.auto()
