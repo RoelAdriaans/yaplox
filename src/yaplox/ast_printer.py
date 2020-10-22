f314ecbd213029acea8b57ea00aade4edd1b3d50
Roel Adriaans <roel@road-support.nl>
2020-10-22 11:27:43 +0200

    Sort ASTprinter, and add missing functions

--- src/yaplox/ast_printer.py
+++ src/yaplox/ast_printer.py
@@ -4,29 +4,28 @@ from yaplox.expr import (
     Call,
     Expr,
     ExprVisitor,
+    Get,
     Grouping,
     Literal,
     Logical,
+    Set,
     Unary,
     Variable,
 )
 
 
 class AstPrinter(ExprVisitor):
-    def visit_call_expr(self, expr: Call):
-        raise NotImplementedError
-
-    def visit_logical_expr(self, expr: Logical):
-        raise NotImplementedError
-
     def visit_assign_expr(self, expr: Assign):
         raise NotImplementedError
 
-    def visit_variable_expr(self, expr: Variable):
+    def visit_binary_expr(self, expr: Binary):
+        return self._parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_call_expr(self, expr: Call):
         raise NotImplementedError
 
-    def print(self, expr: Expr) -> str:
-        return expr.accept(self)
+    def visit_get_expr(self, expr: Get):
+        pass
 
     def visit_grouping_expr(self, expr: Grouping):
         return self._parenthesize("group", expr.expression)
@@ -36,11 +35,20 @@ class AstPrinter(ExprVisitor):
             return "nil"
         return str(expr.value)
 
+    def visit_logical_expr(self, expr: Logical):
+        raise NotImplementedError
+
+    def visit_set_expr(self, expr: Set):
+        pass
+
     def visit_unary_expr(self, expr: Unary):
         return self._parenthesize(expr.operator.lexeme, expr.right)
 
-    def visit_binary_expr(self, expr: Binary):
-        return self._parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    def visit_variable_expr(self, expr: Variable):
+        raise NotImplementedError
+
+    def print(self, expr: Expr) -> str:
+        return expr.accept(self)
 
     def _parenthesize(self, name: str, *args: Expr):
         expressions = ["(", name]
